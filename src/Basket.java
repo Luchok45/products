1d4718a5578dd45f4e0b1f9158f6dca9bca49f61
Luchok45 <125346721+Luchok45@users.noreply.github.com>
2023-04-25 00:16:49 +0300

    first commit

---
+++ src/Basket.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Basket implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String[] goods;
+    private int[] prices;
+    private int[] quantities;
+    public Basket(){}
+    public Basket(String[] goods, int[] price){
+        this.goods = goods;
+        this.prices = prices;
+        this.quantities = new int[goods.length];
+    }
+    public void addToCart(int productNum, int amount){
+        quantities[productNum]+=amount;
+    }
+    public void printCart(){
+        int totalPrice=0;
+        System.out.println("Список продуктов: ");
+        for(int i =0; i < goods.length; i++){
+            if(quantities[i]>0){
+              int currentPrice = prices[i] * quantities[i];
+              totalPrice += currentPrice;
+              System.out.println(i + 1 + "." + " " + goods[i] + " " + quantities[i] + " шт " + currentPrice + " в сумме");
+            }
+        }
+        System.out.println("Сумма вашей покупки: " + totalPrice);
+    }
+    public void saveTxt(File textFile) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            out.println(String.join(" ", goods));
+            out.println(String.join(" ", Arrays.stream(prices)
+                    .mapToObj(String::valueOf)
+                    .toArray(String []::new)));
+            out.println(String.join(" ", Arrays.stream(quantities)
+                    .mapToObj(String::valueOf)
+                    .toArray(String []::new)));
+        }
+
+    }
+            public static Basket loadFromTxtFile(File textFile){
+                Basket basket=new Basket();
+                try(BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))){
+                    String goodsStr = bufferedReader.readLine();
+                    String priceStr = bufferedReader.readLine();
+                    String quantitiesStr = bufferedReader.readLine();
+
+
+                    basket.goods = goodsStr.split(" ");
+                    basket.prices = Arrays.stream(priceStr.split(" "))
+                            .map(Integer::parseInt)
+                            .mapToInt(Integer::intValue)
+                            .toArray();
+                    basket.quantities =Arrays.stream (quantitiesStr.split(" "))
+                            .map(Integer::parseInt)
+                            .mapToInt(Integer::intValue)
+                            .toArray();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return basket;
+            }
+   }
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+       import java.io.File;
+       import java.io.FileNotFoundException;
+       import java.util.Arrays;
+       import java.util.Scanner;
+
+        public class Main {
+            static Scanner scanner = new Scanner(System.in);
+            static String[] products = {"Хлеб", "Яблоки", "Молоко", "Сметана", "Хлопья"};
+            static int[] price = {20, 45, 67, 120, 200};
+            static File saveFile = new File("basket.txt");
+            public static void main(String[] args) throws FileNotFoundException {
+                Basket basket = null;
+                if(saveFile.exists()){
+                    basket = Basket.loadFromTxtFile(saveFile);
+                } else {
+                    basket = new Basket(products, price);
+                }
+                while(true){
+                    showPrice();
+                    System.out.println("Выберите товар и количество или введите `end`");
+                    String input = scanner.nextLine();
+                    if ("end".equals(input)) {
+                        break;
+                    }
+                    String[] part = input.split(" ");
+                    int productNumber = Integer.parseInt(part[0]) - 1;
+                    int productCount = Integer.parseInt(part[1]) - 1;
+                    basket.addToCart(productNumber, productCount);
+                    basket.saveTxt(saveFile);
+                }
+                basket.printCart();
+
+            }
+            public static void showPrice(){
+                System.out.println("Список товаров для покупки: ");
+                for(int i =0; i < products.length; i++){
+                    System.out.println(products[i] + " " + price[i] + " руб/шт");
+                }
+            }
+        }
